928df011abdc38f72df13f5664f40292f81b9b93
dev-by-david <ddugi1214@naver.com>
2022-04-12 00:08:20 +0800

    Push

--- 2022 DIT PS CA3.py
+++ 2022 DIT PS CA3.py
@@ -1,8 +1,15 @@
 from cgitb import text
 from dis import dis
+from importlib.resources import path
+from math import fabs
 from tkinter import *
 from tkinter.messagebox import *
 import re
+import json
+import os.path
+
+db_path = "./ms_database.json"
+# 여기에 검증코드 삽입
 
 def createRegWindow():
     global newRegWindow
@@ -38,9 +45,35 @@ def createRegWindow():
     def inputSwimmerData(event):
         regex = re.compile('^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
         regexMatch = regex.match(entry_birth.get())
-        print(regexMatch)
+        # Validate Birth Data
         if (str(regexMatch) != 'None'):
-            print("OK")
+            if (entry_name.get() != "" and radio_var.get() != 0):
+                # 파일 이미 있는지 여부 검증코드 넣기
+                if os.path.isfile(db_path):
+                    with open(db_path, 'r') as db_json:
+                        db_data = json.load(db_json)
+                    db_data["Active"].append({
+                        "name": entry_name.get(),
+                        "gender": radio_var.get(),
+                        "birth": entry_birth.get()
+                    })
+                    with open(db_path, 'w') as fs:
+                        json.dump(db_data, fs, indent=4)
+                        showinfo("Success", "Registeration Successful")
+                else:
+                    datafile = {}
+                    datafile["Active"] = []
+                    datafile["Active"].append({
+                        "name": entry_name.get(),
+                        "gender": radio_var.get(),
+                        "birth": entry_birth.get()
+                    })
+                    with open(db_path, 'w') as fs:
+                        json.dump(datafile, fs, indent=4)
+                        showinfo("Success", "Database Created, Registeration Successful")
+            else:
+                # Not completely inserted
+                showerror("Error", "Some data is missing.\nPlease input all of the data.")
         else:
             # invalid birth data
             showerror("Error", "There\'s an error in date of birth input.\nInvalid value or incorrect input format.\n(YYYY-MM-DD)")
